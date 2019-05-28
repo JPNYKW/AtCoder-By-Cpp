d7962d00648685d485da36b7fea2222c3076ce98
Haruto Hirakawa <31243896+JPNYKW@users.noreply.github.com>
2019-05-28 15:44:18 +0900

    Create a.cpp

---
+++ ABC/056/a.cpp
@@ -0,0 +1,10 @@
+// https://atcoder.jp/contests/abc058/submissions/5672600
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << (b - a == c - b ? "YES" : "NO") << endl;
+}
